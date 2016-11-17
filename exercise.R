18a49914a7c93f0d96c0f3d53c52753ca4c35056
mohariz <m.rizkyhariz@yahoo.com>
2016-11-17 15:09:53 -0800

    participation

---
+++ exercise.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(dplyr)
+library(data.table)
+
+load("ggplot/suicides.rdata")
+all_suicides <- copy(suicides)
+
+suicides <- suicides %>%
+  group_by(year, state, means) %>% 
+  mutate(deaths = sum(deaths))
+
+## basics
+bare <- ggplot(suicides)
+
+## adding plot aesthetics
+aesthetic <- ggplot(suicides, aes(x=year, y=deaths))
+
+## adding points
+scatter <- ggplot(suicides, aes(x=year, y=deaths)) + geom_point()
+
+## split "means" by color
+color_by_means <- ggplot(suicides, aes(x=year, y=deaths, color=means)) + geom_point()
+
+## facet by state
+state_fact <- ggplot(suicides, aes(x=year, y=deaths, color=means)) + geom_point() + facet_wrap(~state, scales = "free")
+
+## line plot
+line_by_state <- ggplot(suicides, aes(x=year, y=deaths, color=means)) + geom_line(size = 1.5) + facet_wrap(~state, scales = "free")
+
+## bar plot
+state_fact <- ggplot(suicides, aes(x=year, y=deaths, color=means)) + geom_bar(aes(fill=means), stat = "identity") + facet_wrap(~state, scales = "free")
